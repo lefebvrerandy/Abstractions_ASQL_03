9bfa9fad672fdf5a2c8c334c336a27a805fde6cb
Randy Lefebvre <Rlefebvre2256@conestogac.on.ca>
2019-02-19 16:03:00 -0500

    Adding some more control to the second account login. No behind code related to SQL connecting. The buttons all work as they should though. Need to adding SQL support

--- Abstractions_ASQL_03/MainMenuForm.Designer.cs
+++ Abstractions_ASQL_03/MainMenuForm.Designer.cs
@@ -37,8 +37,8 @@
             this.txt_Pass_2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_User_2 = new System.Windows.Forms.Label();
+            this.lbl_Pass_2 = new System.Windows.Forms.Label();
             this.lbl_Error_User_1 = new System.Windows.Forms.Label();
             this.lbl_Error_User_2 = new System.Windows.Forms.Label();
             this.btn_Preview_2 = new System.Windows.Forms.Button();
@@ -47,6 +47,8 @@
             this.btn_Signin_2 = new System.Windows.Forms.Button();
             this.btn_Signin_1 = new System.Windows.Forms.Button();
             this.lbl_Copy_Status = new System.Windows.Forms.Label();
+            this.btn_Second_Account = new System.Windows.Forms.Button();
+            this.lbl_Or = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -55,14 +57,14 @@
             this.comboBox1.Location = new System.Drawing.Point(201, 202);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(189, 33);
-            this.comboBox1.TabIndex = 0;
+            this.comboBox1.TabIndex = 3;
             // 
             // btn_Preview_1
             // 
             this.btn_Preview_1.Location = new System.Drawing.Point(201, 251);
             this.btn_Preview_1.Name = "btn_Preview_1";
             this.btn_Preview_1.Size = new System.Drawing.Size(111, 36);
-            this.btn_Preview_1.TabIndex = 3;
+            this.btn_Preview_1.TabIndex = 4;
             this.btn_Preview_1.Text = "Preview";
             this.btn_Preview_1.UseVisualStyleBackColor = true;
             // 
@@ -71,7 +73,7 @@
             this.btn_Transfer.Location = new System.Drawing.Point(305, 327);
             this.btn_Transfer.Name = "btn_Transfer";
             this.btn_Transfer.Size = new System.Drawing.Size(215, 34);
-            this.btn_Transfer.TabIndex = 6;
+            this.btn_Transfer.TabIndex = 11;
             this.btn_Transfer.Text = "> Copy Database >";
             this.btn_Transfer.UseVisualStyleBackColor = true;
             // 
@@ -80,28 +82,28 @@
             this.txt_User_1.Location = new System.Drawing.Point(212, 77);
             this.txt_User_1.Name = "txt_User_1";
             this.txt_User_1.Size = new System.Drawing.Size(149, 31);
-            this.txt_User_1.TabIndex = 7;
+            this.txt_User_1.TabIndex = 0;
             // 
             // txt_Pass_1
             // 
             this.txt_Pass_1.Location = new System.Drawing.Point(212, 114);
             this.txt_Pass_1.Name = "txt_Pass_1";
             this.txt_Pass_1.Size = new System.Drawing.Size(149, 31);
-            this.txt_Pass_1.TabIndex = 8;
+            this.txt_Pass_1.TabIndex = 1;
             // 
             // txt_User_2
             // 
             this.txt_User_2.Location = new System.Drawing.Point(492, 77);
             this.txt_User_2.Name = "txt_User_2";
             this.txt_User_2.Size = new System.Drawing.Size(124, 31);
-            this.txt_User_2.TabIndex = 9;
+            this.txt_User_2.TabIndex = 5;
             // 
             // txt_Pass_2
             // 
             this.txt_Pass_2.Location = new System.Drawing.Point(492, 114);
             this.txt_Pass_2.Name = "txt_Pass_2";
             this.txt_Pass_2.Size = new System.Drawing.Size(124, 31);
-            this.txt_Pass_2.TabIndex = 10;
+            this.txt_Pass_2.TabIndex = 6;
             // 
             // label1
             // 
@@ -121,23 +123,23 @@
             this.label2.TabIndex = 12;
             this.label2.Text = "Password";
             // 
-            // label3
+            // lbl_User_2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(367, 77);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(119, 25);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "User Name";
+            this.lbl_User_2.AutoSize = true;
+            this.lbl_User_2.Location = new System.Drawing.Point(367, 77);
+            this.lbl_User_2.Name = "lbl_User_2";
+            this.lbl_User_2.Size = new System.Drawing.Size(119, 25);
+            this.lbl_User_2.TabIndex = 13;
+            this.lbl_User_2.Text = "User Name";
             // 
-            // label4
+            // lbl_Pass_2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(380, 114);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(106, 25);
-            this.label4.TabIndex = 14;
-            this.label4.Text = "Password";
+            this.lbl_Pass_2.AutoSize = true;
+            this.lbl_Pass_2.Location = new System.Drawing.Point(380, 114);
+            this.lbl_Pass_2.Name = "lbl_Pass_2";
+            this.lbl_Pass_2.Size = new System.Drawing.Size(106, 25);
+            this.lbl_Pass_2.TabIndex = 14;
+            this.lbl_Pass_2.Text = "Password";
             // 
             // lbl_Error_User_1
             // 
@@ -164,7 +166,7 @@
             this.btn_Preview_2.Location = new System.Drawing.Point(492, 251);
             this.btn_Preview_2.Name = "btn_Preview_2";
             this.btn_Preview_2.Size = new System.Drawing.Size(111, 36);
-            this.btn_Preview_2.TabIndex = 19;
+            this.btn_Preview_2.TabIndex = 10;
             this.btn_Preview_2.Text = "Preview";
             this.btn_Preview_2.UseVisualStyleBackColor = true;
             // 
@@ -174,32 +176,34 @@
             this.comboBox2.Location = new System.Drawing.Point(492, 202);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(189, 33);
-            this.comboBox2.TabIndex = 20;
+            this.comboBox2.TabIndex = 9;
             // 
             // btn_Copy
             // 
-            this.btn_Copy.Location = new System.Drawing.Point(615, 77);
+            this.btn_Copy.Location = new System.Drawing.Point(611, 159);
             this.btn_Copy.Name = "btn_Copy";
-            this.btn_Copy.Size = new System.Drawing.Size(189, 35);
-            this.btn_Copy.TabIndex = 21;
+            this.btn_Copy.Size = new System.Drawing.Size(189, 39);
+            this.btn_Copy.TabIndex = 8;
             this.btn_Copy.Text = "Copy Credential";
             this.btn_Copy.UseVisualStyleBackColor = true;
+            this.btn_Copy.Click += new System.EventHandler(this.btn_Copy_Click);
             // 
             // btn_Signin_2
             // 
             this.btn_Signin_2.Location = new System.Drawing.Point(492, 160);
             this.btn_Signin_2.Name = "btn_Signin_2";
-            this.btn_Signin_2.Size = new System.Drawing.Size(111, 36);
-            this.btn_Signin_2.TabIndex = 22;
+            this.btn_Signin_2.Size = new System.Drawing.Size(111, 39);
+            this.btn_Signin_2.TabIndex = 7;
             this.btn_Signin_2.Text = "Sign In";
             this.btn_Signin_2.UseVisualStyleBackColor = true;
+            this.btn_Signin_2.Click += new System.EventHandler(this.btn_Signin_2_Click);
             // 
             // btn_Signin_1
             // 
             this.btn_Signin_1.Location = new System.Drawing.Point(212, 160);
             this.btn_Signin_1.Name = "btn_Signin_1";
             this.btn_Signin_1.Size = new System.Drawing.Size(111, 36);
-            this.btn_Signin_1.TabIndex = 23;
+            this.btn_Signin_1.TabIndex = 2;
             this.btn_Signin_1.Text = "Sign In";
             this.btn_Signin_1.UseVisualStyleBackColor = true;
             this.btn_Signin_1.Click += new System.EventHandler(this.btn_Signin_1_Click);
@@ -214,11 +218,32 @@
             this.lbl_Copy_Status.TabIndex = 24;
             this.lbl_Copy_Status.Text = "Copy Success";
             // 
+            // btn_Second_Account
+            // 
+            this.btn_Second_Account.Location = new System.Drawing.Point(445, 163);
+            this.btn_Second_Account.Name = "btn_Second_Account";
+            this.btn_Second_Account.Size = new System.Drawing.Size(111, 36);
+            this.btn_Second_Account.TabIndex = 5;
+            this.btn_Second_Account.Text = "Sign In";
+            this.btn_Second_Account.UseVisualStyleBackColor = true;
+            this.btn_Second_Account.Click += new System.EventHandler(this.btn_Second_Account_Click);
+            // 
+            // lbl_Or
+            // 
+            this.lbl_Or.AutoSize = true;
+            this.lbl_Or.Location = new System.Drawing.Point(562, 167);
+            this.lbl_Or.Name = "lbl_Or";
+            this.lbl_Or.Size = new System.Drawing.Size(43, 25);
+            this.lbl_Or.TabIndex = 26;
+            this.lbl_Or.Text = "OR";
+            // 
             // MainMenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_Or);
+            this.Controls.Add(this.btn_Second_Account);
             this.Controls.Add(this.lbl_Copy_Status);
             this.Controls.Add(this.btn_Signin_1);
             this.Controls.Add(this.btn_Signin_2);
@@ -227,8 +252,8 @@
             this.Controls.Add(this.btn_Preview_2);
             this.Controls.Add(this.lbl_Error_User_2);
             this.Controls.Add(this.lbl_Error_User_1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_Pass_2);
+            this.Controls.Add(this.lbl_User_2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_Pass_2);
@@ -248,24 +273,26 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Button btn_Preview_1;
-        private System.Windows.Forms.Button btn_Transfer;
         private System.Windows.Forms.TextBox txt_User_1;
         private System.Windows.Forms.TextBox txt_Pass_1;
+        private System.Windows.Forms.Button btn_Signin_1;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.TextBox txt_User_2;
         private System.Windows.Forms.TextBox txt_Pass_2;
+        private System.Windows.Forms.Button btn_Signin_2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btn_Preview_1;
+        private System.Windows.Forms.Button btn_Transfer;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_User_2;
+        private System.Windows.Forms.Label lbl_Pass_2;
         private System.Windows.Forms.Label lbl_Error_User_1;
         private System.Windows.Forms.Label lbl_Error_User_2;
         private System.Windows.Forms.Button btn_Preview_2;
-        private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.Button btn_Copy;
-        private System.Windows.Forms.Button btn_Signin_2;
-        private System.Windows.Forms.Button btn_Signin_1;
         private System.Windows.Forms.Label lbl_Copy_Status;
+        private System.Windows.Forms.Button btn_Second_Account;
+        private System.Windows.Forms.Label lbl_Or;
     }
 }
--- Abstractions_ASQL_03/MainMenuForm.cs
+++ Abstractions_ASQL_03/MainMenuForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,23 +17,66 @@ namespace Abstractions_ASQL_03
     {
         const int USER_ONE = 1;
         const int USER_TWO = 2;
-        static int HowManyTimes = 0;
 
         public MainMenuForm()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// The first things to load when the window starts
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void MainMenuForm_Load(object sender, EventArgs e)
         {
+            // Hide the status Bars
             lbl_Error_User_1.Hide();
             lbl_Error_User_2.Hide();
             lbl_Copy_Status.Hide();
+
+            // Hide the second user form
+            lbl_User_2.Hide();
+            lbl_Pass_2.Hide();
+            txt_User_2.Hide();
+            txt_Pass_2.Hide();
+            btn_Signin_2.Hide();
+
+            // focus the first text box on load
+            this.ActiveControl = txt_User_1;
         }
 
         private void btn_Signin_1_Click(object sender, EventArgs e)
         {
-            SignInChecker(USER_ONE);   
+            // CHeck that user 1 can successfully connect. If he can, lets prompt the result
+            lbl_Error_User_1.Show();
+            string test = SignInChecker(USER_ONE);
+            if (test != "Failed")
+            {
+                LabelChanger.ChangeText("Successfully Logged in.", lbl_Error_User_1);
+                LabelChanger.ChangeColor("Green", lbl_Error_User_1);
+            }
+            else
+            {
+                LabelChanger.ChangeText("Invalid Credentials.", lbl_Error_User_1);
+                LabelChanger.ChangeColor("Red", lbl_Error_User_1);
+            }
+        }
+
+        private void btn_Signin_2_Click(object sender, EventArgs e)
+        {
+            lbl_Error_User_2.Show();
+            string test = SignInChecker(USER_TWO);
+            if (test != "Failed")
+            {
+                LabelChanger.ChangeText("Successfully Logged in.", lbl_Error_User_2);
+                LabelChanger.ChangeColor("Green", lbl_Error_User_2);
+            }
+            else
+            {
+                LabelChanger.ChangeText("Invalid Credentials.", lbl_User_2);
+                LabelChanger.ChangeColor("Red", lbl_Error_User_2);
+            }
         }
 
         /// <summary>
@@ -44,9 +88,10 @@ namespace Abstractions_ASQL_03
         /// </summary>
         /// <param name="UserNumber">Which User is signing in (1 = Left, 2 = Right)</param>
         /// <returns>Validation</returns>
-        private bool SignInChecker(int UserNumber)
+        private string SignInChecker(int UserNumber)
         {
             bool validation = false;
+            string test = "Failed";
 
             // These fields will be linked to the specific user. User 1 is the left side of the
             // windows form where user 2 is the right side.
@@ -58,17 +103,60 @@ namespace Abstractions_ASQL_03
             {
                 // Send to "Check Credentials method and return the status to see if it was valid
 
+            //DEBUG
+                OleDbConnection conn = new OleDbConnection();
+                //DEBUG
+
                 // if valid, change label to text = Success, and color = Green
                 // update combobox 1 with all the tables from the required user
 
-                // else change label text = Invalid Credentials, color = Red
+                test = "StringToPass(ConnectionString)";
+
             }
             else
             {
-                validation = false;
+                test = "Failed";
             }
 
-            return validation;
+            return test;
         }
+
+        private void btn_Second_Account_Click(object sender, EventArgs e)
+        {
+            // Hide all unrelated field
+            lbl_Or.Hide();
+            btn_Second_Account.Hide();
+
+            // Show related fields
+            lbl_User_2.Show();
+            lbl_Pass_2.Show();
+            txt_User_2.Show();
+            txt_Pass_2.Show();
+            btn_Signin_2.Show();
+
+            // Set the focus to the new field User Name
+            this.ActiveControl = txt_User_2;
+        }
+
+        private void btn_Copy_Click(object sender, EventArgs e)
+        {
+            // Hide the second user form
+            lbl_User_2.Hide();
+            lbl_Pass_2.Hide();
+            txt_User_2.Hide();
+            txt_Pass_2.Hide();
+            btn_Signin_2.Hide();
+
+            // Show related fields
+            btn_Second_Account.Show();
+            lbl_Or.Show();
+
+            // Retreive information from the first sign in form
+            // If successfully logged in, display success
+
+            // else if failed to log in, display error
+        }
+
+
     }
 }
