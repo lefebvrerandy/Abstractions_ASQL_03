8d517838ef83416daade48febae013ef5a8973d1
Randy Lefebvre <Rlefebvre2256@conestogac.on.ca>
2019-02-19 18:57:41 -0500

    Home push.

--- Abstractions_ASQL_03/Form1.Designer.cs
+++ Abstractions_ASQL_03/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txt_UserName = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.lbl_UserName = new System.Windows.Forms.Label();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.btn_SignIn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt_UserName
+            // 
+            this.txt_UserName.Location = new System.Drawing.Point(125, 28);
+            this.txt_UserName.Name = "txt_UserName";
+            this.txt_UserName.Size = new System.Drawing.Size(100, 20);
+            this.txt_UserName.TabIndex = 0;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(125, 81);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(100, 20);
+            this.txt_Password.TabIndex = 1;
+            // 
+            // lbl_UserName
+            // 
+            this.lbl_UserName.AutoSize = true;
+            this.lbl_UserName.Location = new System.Drawing.Point(125, 9);
+            this.lbl_UserName.Name = "lbl_UserName";
+            this.lbl_UserName.Size = new System.Drawing.Size(60, 13);
+            this.lbl_UserName.TabIndex = 2;
+            this.lbl_UserName.Text = "User Name";
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.Location = new System.Drawing.Point(125, 65);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(53, 13);
+            this.lbl_Password.TabIndex = 3;
+            this.lbl_Password.Text = "Password";
+            // 
+            // btn_SignIn
+            // 
+            this.btn_SignIn.Location = new System.Drawing.Point(138, 118);
+            this.btn_SignIn.Name = "btn_SignIn";
+            this.btn_SignIn.Size = new System.Drawing.Size(75, 23);
+            this.btn_SignIn.TabIndex = 4;
+            this.btn_SignIn.Text = "Sign In";
+            this.btn_SignIn.UseVisualStyleBackColor = true;
+            this.btn_SignIn.Click += new System.EventHandler(this.btn_SignIn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Sign in to your SQL database.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(349, 324);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_SignIn);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.lbl_UserName);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_UserName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_UserName;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label lbl_UserName;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Button btn_SignIn;
+        private System.Windows.Forms.Label label1;
     }
 }
 
---
+++ Abstractions_ASQL_03/SQL.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Abstractions_ASQL_03
+{
+    class SQL
+    {
+        private string userName;
+        private string password;
+
+
+        public bool CheckAuthentication(string userName, string password, ref string[] returnResult)
+        {
+            bool valid = false;
+
+            SqlConnectionStringBuilder connString = new SqlConnectionStringBuilder();
+            connString.DataSource = "localhost";
+            connString.UserID = userName;
+            connString.Password = password;
+            connString.InitialCatalog = "master";
+
+            using (SqlConnection conn = new SqlConnection(connString.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    valid = true;
+                    returnResult[0] = "Connecting...";
+                }
+                catch(SqlException e)
+                {
+                    valid = false;
+                    //string exceptionToSplit = e.ToString();
+                    //string[] resultArray = exceptionToSplit.Split('\n');
+                    //returnResult = resultArray[0];
+                    if (e.Errors.Count <= 5)
+                    {
+                        for( int i = 0; i < e.Errors.Count; i++)
+                        { 
+                            returnResult[i] = e.Errors[i].Message;
+                            i++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 5; i++)
+                        {
+                            returnResult[i] = e.Errors[i].Message;
+                            i++;
+                        }
+                    }
+
+                }
+
+                return valid;
+            }
+        }
+
+    }
+}
